560426dc00dd629173c1670a72bc60b3ee4f6836
Paras D Sutariya <paras.sutariya125@gmail.com>
2022-12-02 01:55:35 +0530

    Assignment 4.1 Done custom Promise function

--- Week-04/Assignment 4.1/custom_promise.js
+++ Week-04/Assignment 4.1/custom_promise.js
@@ -1,15 +1,52 @@
-
-const getNumber = () => {
-  let number = Math.random();
-  //number = 5;
-  if (number % 5 == 0) {
-    console.log(`Promise is Rejected for number ${number}`);
-    return false
-  } else {
-    console.log(`Promise is Resolve for number ${number}`);
-    return true;
+class customPromise {
+  constructor(fn) {
+    this.status = "pending";
+    this.resolve = this.resolve.bind(this);
+    this.reject = this.reject.bind(this);
+    this.catchCallback = null;
+    this.thenCallback = null;
+    fn(this.resolve, this.reject);
   }
-  
-};
 
-console.log(getNumber());
+  resolve(resolvedData) {
+    console.log("Resolve function called");
+    this.thenCallback(resolvedData);
+    this.status = "resolve";
+  }
+
+  reject(rejectedData) {
+    console.log("Reject function called");
+    this.catchCallback(rejectedData);
+    this.status = "rejected";
+  }
+
+  then(thenFn) {
+    console.log("Then Function called");
+    this.thenCallback = thenFn;
+    return this;
+  }
+
+  catch(catchFn) {
+    console.log("Catch Function called");
+    this.catchCallback = catchFn;
+    return this;
+  }
+}
+
+function getNumber() {
+  return new customPromise((res, rej) => {
+    const randomNum = Math.floor(Math.random() * 100);
+    // const randomNum = 5;
+    setTimeout(() => {
+      if (randomNum % 5 === 0) {
+        rej(`Promise Reject With Number ${randomNum}`);
+      } else {
+        res(`Promise Resolve With Number ${randomNum}`);
+      }
+    }, 1000);
+  });
+}
+
+getNumber()
+  .then((val) => console.log(val))
+  .catch((val) => console.log(val));
