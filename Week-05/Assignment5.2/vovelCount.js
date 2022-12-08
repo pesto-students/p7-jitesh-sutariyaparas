4a638a6263a3793d577dfc2fe80eabb859b2fb42
Paras D Sutariya <paras.sutariya125@gmail.com>
2022-12-08 09:45:47 +0530

    Improve vovel check script

--- Week-05/Assignment5.2/vovelCount.js
+++ Week-05/Assignment5.2/vovelCount.js
@@ -1,10 +1,6 @@
 // Write a function called vowelCount which accepts a string and returns a map where
 // the keys are numbers and the values are the count of the vowels in the string.
 
-// const checkIsVovel = (char) => {
-//   return "aeiou".includes(char);
-// };
-
 const vowelCount = (myString) => {
   const vowelMap = new Map();
   myString = myString.toLowerCase();
