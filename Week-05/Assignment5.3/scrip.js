1ab6ce1ecc5988a6a9c0d3fff88ea70140f64f6f
Paras D Sutariya <paras.sutariya125@gmail.com>
2022-12-08 09:43:00 +0530

    Improve Array duplicate check fn

--- Week-05/Assignment5.3/scrip.js
+++ Week-05/Assignment5.3/scrip.js
@@ -1,12 +1,8 @@
 // Write a function called hasDuplicate which accepts an array and returns true or false if that array contains a duplicate
 
 const hasDuplicate = (myArray) => {
-  let setArray = new Set(myArray);
-  if (setArray.size == myArray.length) {
-    return false;
-  } else {
-    return true;
-  }
+  const hasDuplicate = (arr) => new Set(myArray).size !== myArray.length;
+  return hasDuplicate;
 };
 
 console.log(hasDuplicate([1, 3, -3, 5, 6, 7]));
