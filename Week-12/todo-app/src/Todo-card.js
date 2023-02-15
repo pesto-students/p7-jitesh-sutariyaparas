17d78f5de5878dd97edbe949864aa29cc2c4f2ae
Paras D Sutariya <paras.sutariya125@gmail.com>
2023-02-15 15:06:44 +0530

    Update todo form view design

--- Week-12/todo-app/src/Todo-card.js
+++ Week-12/todo-app/src/Todo-card.js
@@ -1,4 +1,4 @@
-// import "./Todo-list.css";
+import "./Todo-list.css";
 // import React, { useState } from "react";
 
 const TodoCard = ({ values, removeNote, doneNote, editNote }) => {
@@ -14,14 +14,42 @@ const TodoCard = ({ values, removeNote, doneNote, editNote }) => {
 
   return (
     <>
-      <div className="todo-list">
-        Title:{values.name}
-        Description:{values.description}
-        ID:{values.id}
-        status:{values.status}
-        <button onClick={removeCard}>DELETE</button>
-        <button onClick={doneCard}>DONE</button>
-        <button onClick={editCard}>EDIT</button>
+      <div className="todo-card">
+        <div>
+          <span>
+            <h4>{values.name}</h4>
+          </span>
+          <span className="badge badge-success">{values.status}</span>
+        </div>
+        <div> Description:{values.description}</div>
+
+        <div class="btn btn-primary btn-sm">
+          <i
+            Style="font-size:14px;"
+            class="fas fa-edit"
+            onClick={editCard}
+          > Edit
+          </i>
+        </div>
+
+        <div class="btn btn-success btn-sm">
+          <i
+            Style="font-size:14px;"
+            class="fas fa-check"
+            onClick={doneCard}
+          > Approve
+          </i>
+        </div>
+
+        <div class="btn btn-danger btn-sm">
+          <i
+            Style="font-size:14px;"
+            class="fas fa-trash"
+            onClick={removeCard}
+          > Delete
+          </i>
+        </div>
+        
       </div>
     </>
   );
--- Week-12/todo-app/src/Todo-form.js
+++ Week-12/todo-app/src/Todo-form.js
@@ -1,5 +1,7 @@
 import "./Todo-form.css";
 import React, { useState } from "react";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faTimes  } from '@fortawesome/free-solid-svg-icons';
 
 const TodoForm = ({ createNote, closeTodoForm, values, actionUpdate }) => {
   const [name, setName] = useState(values.name);
@@ -19,31 +21,63 @@ const TodoForm = ({ createNote, closeTodoForm, values, actionUpdate }) => {
   return (
     <div className="todo-box">
       <div className="todo-form">
-        <button onClick={() => closeTodoForm()}>Close</button>
+        <div class="btn close-button">
+          <i
+            Style="color:red;font-size:25px;"
+            class="fas fa-times-circle"
+            onClick={() => closeTodoForm()}
+          ></i>
+        </div>
+
         <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="textarea"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          {values.id ? <></> : <input type="submit" value="Add note" />}
-          <button
-            onClick={(event) => {
-              const newValues = values;
-              newValues["name"] = name;
-              newValues["description"] = description;
-              actionUpdate(newValues);
-              console.log("UPDATE BUTTON CLICK", name, description, values);
-              event.preventDefault();
-            }}
-          >
-            UPDATE
-          </button>
+          <h2>Note</h2>
+
+          <div className="form-group">
+            <label for="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              placeholder="Enter your name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label for="description">Description</label>
+            <textarea
+              id="description"
+              value={description}
+              rows="5"
+              placeholder="Enter a description"
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          {values.id ? (
+            <></>
+          ) : (
+            <input
+              type="submit"
+              className="btn btn-success"
+              value="Save"
+            ></input>
+          )}
+          {values.id ? (
+            <button
+              className="btn btn-primary"
+              onClick={(event) => {
+                const newValues = values;
+                newValues["name"] = name;
+                newValues["description"] = description;
+                actionUpdate(newValues);
+                console.log("UPDATE BUTTON CLICK", name, description, values);
+                event.preventDefault();
+              }}
+            >
+              UPDATE
+            </button>
+          ) : (
+            <></>
+          )}
         </form>
       </div>
     </div>
