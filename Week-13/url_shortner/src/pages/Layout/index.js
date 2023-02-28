8fa84bc32457418f44270a8d88aa50cf40a63cd3
Paras D Sutariya <paras.sutariya125@gmail.com>
2023-03-01 00:24:58 +0530

    useLocation to get active tab

--- Week-13/url_shortner/src/pages/Layout/index.js
+++ Week-13/url_shortner/src/pages/Layout/index.js
@@ -1,21 +1,37 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import Footer from "../../components/Footer";
 import "./layout.css";
 
-
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <>
       <nav>
         <ul className="nav-header">
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              className={location.pathname === "/" ? "nav-active" : ""}
+              to="/"
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              className={location.pathname === "/about" ? "nav-active" : ""}
+              to="/about"
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              className={location.pathname === "/contact" ? "nav-active" : ""}
+              to="/contact"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
