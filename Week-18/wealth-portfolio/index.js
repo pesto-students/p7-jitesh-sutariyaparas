596e6f7672b236a1c0a6ac64b7a92e902b2da4d3
Paras D Sutariya <paras.sutariya125@gmail.com>
2023-03-18 02:31:31 +0530

    Add mongoDB connection

--- Week-18/wealth-portfolio/index.js
+++ Week-18/wealth-portfolio/index.js
@@ -1,5 +1,19 @@
 var express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db.config.js");
 const app = express();
+require("dotenv").config();
 const PORT = 3005;
 
+// connecting to db
+connectDB();
 
+app.use(express.json());
+
+app.use(function (req, res) {
+  res.status(404).json("err: Page not found");
+});
+
+app.listen(PORT, () => {
+  console.log("app is running on port " + PORT);
+});
