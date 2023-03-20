1b7cdc338d38da75f29e9aa76ef4d369cf815951
Paras D Sutariya <paras.sutariya125@gmail.com>
2023-03-20 19:31:48 +0530

    Add User login api

--- Week-18/wealth-portfolio/controllers/user.controller.js
+++ Week-18/wealth-portfolio/controllers/user.controller.js
@@ -18,7 +18,7 @@ const getUsers = async (req, res) => {
 
 const createUser = async (req, res) => {
   try {
-    console.log("req.body__",req.body)
+    console.log("req.body__", req.body);
     const users = await User.findOne({ email: req.body.email });
     if (users) {
       return res.json({
@@ -33,7 +33,41 @@ const createUser = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  console.log("__",email,password)
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      console.log("user.password__", password, user.password);
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        console.log(user);
+        res.json({
+          user: {
+            id: user._id,
+            email: user.email,
+            firstname: user.firstname,
+            lastname: user.lastname,
+          },
+          access_token: jwt.sign(
+            { id: req.body.id, email },
+            process.env.JWT_SECRET
+          ),
+        });
+      } else {
+        res.json("password does not match");
+      }
+    } else {
+      res.json("User does not exist");
+    }
+  } catch (e) {
+    console.log(err);
+  }
+};
+
 module.exports = {
   getUsers,
   createUser,
+  login,
 };
--- Week-18/wealth-portfolio/routes/user.route.js
+++ Week-18/wealth-portfolio/routes/user.route.js
@@ -8,5 +8,6 @@ const authMiddleware = require("../middleware/auth");
 
 router.get("/api/users", authMiddleware, userController.getUsers);
 router.post('/api/users', userController.createUser)
+router.post('/api/user/login', userController.login)
 
 module.exports = router;
