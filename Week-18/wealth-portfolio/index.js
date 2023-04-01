c4b1df35eb2890c2f6d793d7dcb89f1703c016c7
Paras D Sutariya <paras.sutariya125@gmail.com>
2023-04-02 01:07:59 +0530

    Add swaggerUi api

--- Week-18/wealth-portfolio/index.js
+++ Week-18/wealth-portfolio/index.js
@@ -1,14 +1,23 @@
 var express = require("express");
-const mongoose = require("mongoose");
 const connectDB = require("./config/db.config.js");
 const app = express();
+app.use(express.json());
 require("dotenv").config();
 const PORT = 3005;
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// const router = require('express').Router()
+// router.use('/api-docs', swaggerUi.serve);
+// router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 
 // connecting to db
 connectDB();
 
-app.use(express.json());
+
 app.use("/v1", require("./routes/user.route"));
 app.use("/v1", require("./routes/asset.route"));
 app.use("/v1", require("./routes/income.route"));
@@ -16,6 +25,7 @@ app.use("/v1", require("./routes/expense.route"));
 app.use("/v1", require("./routes/saving.route"));
 app.use("/v1", require("./routes/equity.route"));
 app.use("/v1", require("./routes/fixedIncome.route"));
+
 
 app.use(function (req, res) {
   res.status(404).json("err: Page not found");
