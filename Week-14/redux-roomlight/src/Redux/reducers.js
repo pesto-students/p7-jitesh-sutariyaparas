389d0e2aa99cad9a7ebae58195974c23a6d87ef1
Paras D Sutariya <paras.sutariya125@gmail.com>
2023-04-15 03:13:06 +0530

    Add redux roomlight app and step counterapp

---
+++ Week-14/redux-roomlight/src/Redux/reducers.js
@@ -0,0 +1,17 @@
+const initialState = {
+  lightStatus: false,
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "TOGGLE_LIGHT":
+      return {
+        ...state,
+        lightStatus: !state.lightStatus,
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
